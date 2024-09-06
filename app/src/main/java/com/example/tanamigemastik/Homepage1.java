e8d79eb91b5baad37ad7de99abbe9c2978e72fc2
Fadiil <fadilthoriq201@gmail.com>
2024-09-06 08:21:22 +0700

    tanami coin page progress-1

--- app/src/main/java/com/example/tanamigemastik/Homepage1.java
+++ app/src/main/java/com/example/tanamigemastik/Homepage1.java
@@ -2,7 +2,11 @@ package com.example.tanamigemastik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Homepage1 extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class Homepage1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage1);
+
+        // Inisialisasi TextView
+        TextView tanamiCoinTextView = findViewById(R.id.tanami_coin);
+
+        // Set OnClickListener pada TextView
+        tanamiCoinTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent untuk membuka halaman TanamiCoin
+                Intent intent = new Intent(Homepage1.this, TanamiCoin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
