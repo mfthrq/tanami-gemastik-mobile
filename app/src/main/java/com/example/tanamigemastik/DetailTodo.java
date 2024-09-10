2a18b80d36d57bcce4ced65b3dfe16b87ff5c1b2
Fadiil <fadilthoriq201@gmail.com>
2024-09-10 07:42:59 +0700

    cek hasil tanam dan ubah icon apps dan nama apps done

--- app/src/main/java/com/example/tanamigemastik/DetailTodo.java
+++ app/src/main/java/com/example/tanamigemastik/DetailTodo.java
@@ -2,7 +2,10 @@ package com.example.tanamigemastik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DetailTodo extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class DetailTodo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_todo);
+
+        // Find ImageView by ID
+        ImageView cekTanamanRealTime = findViewById(R.id.cek_tanaman_real_time);
+
+        // Set an OnClickListener for the ImageView
+        cekTanamanRealTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to CekHasilTanaman activity
+                Intent intent = new Intent(DetailTodo.this, CekHasilTanaman.class);
+                startActivity(intent);
+            }
+        });
     }
 }
