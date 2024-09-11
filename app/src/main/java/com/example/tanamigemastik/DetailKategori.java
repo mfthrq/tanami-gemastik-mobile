fa86966291b5738b74febad9177553758454077f
Fadiil <fadilthoriq201@gmail.com>
2024-09-11 19:55:31 +0700

    detail kategori done-1

--- app/src/main/java/com/example/tanamigemastik/DetailKategori.java
+++ app/src/main/java/com/example/tanamigemastik/DetailKategori.java
@@ -2,26 +2,16 @@ package com.example.tanamigemastik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class KategoriProduk extends AppCompatActivity {
+public class DetailKategori extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kategori_produk);
-
-        ImageView tanamanBuah = findViewById(R.id.tanaman_buah);
-        tanamanBuah.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(KategoriProduk.this, DetailKategori.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_detail_kategori);
 
         // Initialize the back button
         ImageView tombolBack = findViewById(R.id.tombol_back);
